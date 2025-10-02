082d8bf2d5e76ab6538693b99223a2f6029d3369
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2025-10-02 23:12:56 +0900

    로그인 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import ReservationMenuPage from './pages/ReservationMenuPage';
 import ReservationFormSelectPage from './pages/ReservationFormSelectPage';
 import PlaceFocusSelectPage from './pages/PlaceFocusSelectPage';
 import ReservationDetailsPage from './pages/ReservationDetailsPage';
+import LoginPage from './pages/LoginPage';
 function App() {
-  const [page, setPage] = useState('main');
+  const [page, setPage] = useState('loginPage');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);// 로그인 상태 관리
+
 
   const renderPage = () => {
     switch (page) {
@@ -17,11 +20,19 @@ function App() {
         return <ReservationFormSelectPage onNavigate={setPage} />;
       case 'placeFocusSelect':
         return <PlaceFocusSelectPage onNavigate={setPage} />;
-      case 'ReservationDetailsPage':
+      case 'reservationDetailsPage':
         return <ReservationDetailsPage onNavigate={setPage} />;
+      case 'loginPage':
+        return <LoginPage onNavigate={setPage} handleLogin={handleLoginSuccess} />;
       default:
         return <MainPage onNavigate={setPage} />;
     }
+
+
+  };
+  const handleLoginSuccess = (status) => {
+    setIsLoggedIn(status); // 로그인 상태 업데이트
+    setPage('main');       // ★ 'navigate' 대신 'setPage'를 사용하여 페이지 전환
   };
 
   return (
